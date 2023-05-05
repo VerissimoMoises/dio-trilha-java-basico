4bee4aaf77daf3c8724099d99e360f3503790a4e
Moisés Verissimo de Souza <102416201+VerissimoMoises@users.noreply.github.com>
2023-05-05 17:07:04 -0300

    Entendendo variaveis de tipos primitivos em Java

---
+++ TIPOS-VARIAVEIS/src/TiposVariaveis.java
@@ -0,0 +1,15 @@
+// Tipos primitivos: int (4 bytes), byte (1 byte), short (2 byte), long (8 bytes), float (4 bytes), double (8 bytes), boolean e char
+
+public class TiposVariaveis {
+    
+    public static void main(String[] args) {
+
+        // byte idade = 123;
+        // short ano = 2023;
+        // int cep = 21070333;
+        // long cpf = 98765432109L; // O L serve para a linguagem entender o tipo como long
+        // float pi = 3.14F; // O F serve para a linguagem entender o tipo como float
+        // double salarioMinimo = 2500.33;
+
+    }
+}
