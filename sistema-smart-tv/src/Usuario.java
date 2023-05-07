32c0a91c8c8b88f6c8eb1ee65b8ec5368d324461
Moisés Verissimo de Souza <102416201+VerissimoMoises@users.noreply.github.com>
2023-05-07 11:18:05 -0300

    Meu projeto conta banco

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -1,24 +1,9 @@
 public class Usuario {
+    
+
     public static void main(String[] args) throws Exception {
-        
-        SmartTv smartTv = new SmartTv();
-
-        smartTv.diminuirVolume();
-        smartTv.diminuirVolume();
-        smartTv.diminuirVolume();
-        smartTv.aumentarVolume();
-
-        smartTv.mudarCanal(13);
-        System.out.println("Canal atual: " + smartTv.canal);
-
-        System.out.println("TV ligada? " + smartTv.ligada);
-        System.out.println("Volume atual: " + smartTv.volume);
-
-        smartTv.ligar();
-        System.out.println("Novo status => TV ligada? " + smartTv.ligada);
-
-        smartTv.desligar();
-        System.out.println("Novo status => TV ligada? " + smartTv.ligada);
-        
-    }
+        int numero1 = 1;
+        String numero2 = "2";
+        System.out.println(numero1+numero2); 
+      }
 }
