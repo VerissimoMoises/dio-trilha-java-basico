4e7c6c7bcc6408e33c0ba54e8fe306709420fefb
Moisés Verissimo de Souza <102416201+VerissimoMoises@users.noreply.github.com>
2023-05-07 11:38:36 -0300

    Código comentado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 public class ContaTerminal {
 
     public static void main(String[] args) throws Exception{
-        // TODO: Conhecer e importar a classe Scanner
+        /*  TODO: Conhecer e importar a classe Scanner
+            Exibir as mensagens para o nosso usuário 
+            Obeter pela classe scanner os valores digitados no terminal
+            Exibir a mensagem da conta criada
+        */
 
-        // Exibir as mensagens para o nosso usuário 
-
-        // Obeter pela classe scanner os valores digitados no terminal
-
-        // Exibir a mensagem da conta criada
-
+        //Declarando a variavel Scanner para poder receber os dados dos clientes pelo terminal
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
+        // Exibindo as mensagens para inserção dos dados e em seguida declarando as cariaveis onde serão armazenados
         System.out.println("Por favor, informe o numero da conta: ");
         int numero = scanner.nextInt();
 
@@ -26,6 +26,7 @@ public class ContaTerminal {
         System.out.println("Qual o valor do saldo?");
         Double saldo = scanner.nextDouble();
 
+        // Exibindo a mensagem de conta criada com os dados fornecidos pelos clientes
         System.out.println("Olá " + nomeCliente + ", obrigado(a) por criar uma conta em nosso banco, sua gência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + "já está disponível para saque.");
     }
     
